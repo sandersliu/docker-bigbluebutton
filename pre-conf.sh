eb8108fe83c67a972be167f55b94fb19db332dd3
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-15 10:55:44 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -4,3 +4,20 @@
 #reason of this script is that dockerfile only execute one command at the time but we need sometimes at the moment we create 
 #the docker image to run more that one software for expecified configuration like when you need mysql running to chnage or create
 #database for the container ...
+
+apt-get update
+apt-get install -y -q -f --force-yes bigbluebutton 
+
+
+
+
+
+
+
+
+
+
+
+apt-get clean 
+rm -rf /tmp/* /var/tmp/*  
+rm -rf /var/lib/apt/lists/*
