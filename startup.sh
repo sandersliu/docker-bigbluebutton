ef23a6c405276afbba8744db4c692cad0a55aea6
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-16 10:40:00 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -6,14 +6,14 @@ set -e
 if [ -f /etc/configured ]; then
         #to make sure it works ..
         
-        bbb-conf --check
+        #bbb-conf --check
         echo 'already configured'
 else
         #code that need to run only one time ....
         #needed for fix problem with ubuntu and cron
-        dpkg --configure -a
-        bbb-conf --setip $(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-        bbb-conf --check
+        #dpkg --configure -a
+        #bbb-conf --setip $(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
+        #bbb-conf --check
         update-locale 
         date > /etc/configured
 fi
