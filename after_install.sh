bcf5d7c87e509ed4471513706728647bce2474d7
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-26 17:07:10 -0400

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -4,10 +4,12 @@
 #where it will run.... need to find all the wrong conf file and fix it in this area ...
 ctner_intn_ip=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 #need to find external ip address of server 
+#this fail to get the right ip when inside of home network ... it get the ip from router ..outside of home....
 ctner_extn_ip=$(curl -s icanhazip.com)
 #to configured bigbluebutton
 bbb-conf --setip $ctner_intn_ip
 #to change conf /etc/bigbluebutton/nginx/sip.nginx
+# this fail too .....
 sed -i 's/.*proxy_pass.http:.*/proxy_pass http:\/$ctner_extn_ip\/:5066;/' /etc/bigbluebutton/nginx/sip.nginx
 #to change conf /opt/freeswitch/conf/vars.xml
 sed -i 's/.*data="local_ip_v4.*/ /' /opt/freeswitch/conf/vars.xml
@@ -15,6 +17,7 @@ sed -i 's/.*data="bind_server_ip.*/<X-PRE-PROCESS cmd="set" data="bind_server_ip
 sed -i 's/.*data="external_rtp_ip.*/<X-PRE-PROCESS cmd="set" data="external_rtp_ip='$ctner_extn_ip'"\/>/' /opt/freeswitch/conf/vars.xml
 sed -i 's/.*data="external_sip_ip.*/<X-PRE-PROCESS cmd="set" data="external_sip_ip='$ctner_extn_ip'"\/>/' /opt/freeswitch/conf/vars.xml
 #to change conf /opt/freeswitch/conf/sip_profiles/external.xml
+#this fail
 sed -i 's/.*name="rtp-ip" value.*/<param name="rtp-ip" value="'$ctner_intn_ip'"/>/' /opt/freeswitch/conf/sip_profiles/external.xml
 sed -i 's/.*name="sip-ip" value.*/<param name="sip-ip" value="'$ctner_intn_ip'"/>/' /opt/freeswitch/conf/sip_profiles/external.xml
 sed -i 's/.*name="ext-rtp-ip" value.*/<param name="ext-rtp-ip" value="'$ctner_extn_ip'"/>/' /opt/freeswitch/conf/sip_profiles/external.xml
