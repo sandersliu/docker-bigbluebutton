1bfa8f4bf0831b72b0b0c345e434584db40ad916
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-26 10:34:01 -0400

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-
 #take the container real ip to configured the web application 
 # in some areas fail because it keep ip info where the container was build that is not the same 
 #where it will run.... need to find all the wrong conf file and fix it in this area ...
 ctner_intn_ip=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-ctner_extn_ip=$(curl -s icanhazip.com)
-
-bbb-conf --setip $ctner_intn_ip
-
 #need to find external ip address of server 
-#Need to fix 
-#/etc/bigbluebutton/nginx/sip.nginx
-# proxy_pass http://172.17.0.193:5066;   # replace line... external maybe
-
+ctner_extn_ip=$(curl -s icanhazip.com)
+#to configured bigbluebutton
+bbb-conf --setip $ctner_intn_ip
+#to change conf /etc/bigbluebutton/nginx/sip.nginx
+sed -i 's/.*proxy_pass.http:.*/proxy_pass http:\/$ctner_extn_ip\/:5066;/' /etc/bigbluebutton/nginx/sip.nginx
 #to change conf /opt/freeswitch/conf/vars.xml
 sed -i 's/.*data="local_ip_v4.*/ /' /opt/freeswitch/conf/vars.xml
 sed -i 's/.*data="bind_server_ip.*/<X-PRE-PROCESS cmd="set" data="bind_server_ip='$ctner_extn_ip'"\/>/' /opt/freeswitch/conf/vars.xml
@@ -23,22 +19,13 @@ sed -i 's/.*name="rtp-ip" value.*/<param name="rtp-ip" value="'$ctner_intn_ip'"/
 sed -i 's/.*name="sip-ip" value.*/<param name="sip-ip" value="'$ctner_intn_ip'"/>/' /opt/freeswitch/conf/sip_profiles/external.xml
 sed -i 's/.*name="ext-rtp-ip" value.*/<param name="ext-rtp-ip" value="'$ctner_extn_ip'"/>/' /opt/freeswitch/conf/sip_profiles/external.xml
 sed -i 's/.*name="ext-sip-ip" value.*/<param name="ext-sip-ip" value="'$ctner_extn_ip'"/>/' /opt/freeswitch/conf/sip_profiles/external.xml
-
-##########
-Edit /usr/share/red5/webapps/sip/WEB-INF/bigbluebutton-sip.properties
-
-bbb.sip.app.ip=<internal ip>
-bbb.sip.app.port=5070
-
-freeswitch.ip=<internal ip>
-freeswitch.port=5060
-
-####################
-/var/lib/tomcat7/webapps/demo/bbb_api_conf.jsp
-String BigBlueButtonURL = "http://199.59.90.34:49166/bigbluebutton/";  external ip:port 
-#####################
-/etc/nginx/sites-available/bigbluebutton   internal port ....
-#####################
-#Open the firewall (if you have on installed) and Security Groups (if your using EC2) the following ports:
+#to change conf /usr/share/red5/webapps/sip/WEB-INF/bigbluebutton-sip.properties
+sed -i 's/.*bbb.sip.app.ip=.*/bbb.sip.app.ip=$ctner_intn_ip/' /usr/share/red5/webapps/sip/WEB-INF/bigbluebutton-sip.properties
+sed -i 's/.*freeswitch.ip=.*/freeswitch.ip=$ctner_intn_ip/' /usr/share/red5/webapps/sip/WEB-INF/bigbluebutton-sip.properties
+#to change /var/lib/tomcat7/webapps/demo/bbb_api_conf.jsp
+sed -i 's/.*String.BigBlueButtonURL.*/String BigBlueButtonURL = "http:\/\/'$ctner_extn_ip'\/bigbluebutton\/";/' /var/lib/tomcat7/webapps/demo/bbb_api_conf.jsp
+#to change /etc/nginx/sites-available/bigbluebutton
+#this maybe if the port will no be the same ...
+#Open the firewall (if you have on installed) and Security Groups the following ports:
 #TCP - 5066
 #UDP - 16384 to 32768
