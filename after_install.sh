600970ae6de81128d2befbf1fe8bd81980fb20e1
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-26 17:25:35 -0400

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -9,22 +9,20 @@ ctner_extn_ip=$(curl -s icanhazip.com)
 #to configured bigbluebutton
 bbb-conf --setip $ctner_intn_ip
 #to change conf /etc/bigbluebutton/nginx/sip.nginx
-# this fail too .....
-sed -i 's/.*proxy_pass.http:.*/proxy_pass http:\/$ctner_extn_ip\/:5066;/' /etc/bigbluebutton/nginx/sip.nginx
+sed -i "s/.*proxy_pass.http:.*/proxy_pass http:\/\/$ctner_extn_ip:5066;/" /etc/bigbluebutton/nginx/sip.nginx
 #to change conf /opt/freeswitch/conf/vars.xml
 sed -i 's/.*data="local_ip_v4.*/ /' /opt/freeswitch/conf/vars.xml
 sed -i 's/.*data="bind_server_ip.*/<X-PRE-PROCESS cmd="set" data="bind_server_ip='$ctner_extn_ip'"\/>/' /opt/freeswitch/conf/vars.xml
 sed -i 's/.*data="external_rtp_ip.*/<X-PRE-PROCESS cmd="set" data="external_rtp_ip='$ctner_extn_ip'"\/>/' /opt/freeswitch/conf/vars.xml
 sed -i 's/.*data="external_sip_ip.*/<X-PRE-PROCESS cmd="set" data="external_sip_ip='$ctner_extn_ip'"\/>/' /opt/freeswitch/conf/vars.xml
 #to change conf /opt/freeswitch/conf/sip_profiles/external.xml
-#this fail
-sed -i 's/.*name="rtp-ip" value.*/<param name="rtp-ip" value="'$ctner_intn_ip'"/>/' /opt/freeswitch/conf/sip_profiles/external.xml
-sed -i 's/.*name="sip-ip" value.*/<param name="sip-ip" value="'$ctner_intn_ip'"/>/' /opt/freeswitch/conf/sip_profiles/external.xml
-sed -i 's/.*name="ext-rtp-ip" value.*/<param name="ext-rtp-ip" value="'$ctner_extn_ip'"/>/' /opt/freeswitch/conf/sip_profiles/external.xml
-sed -i 's/.*name="ext-sip-ip" value.*/<param name="ext-sip-ip" value="'$ctner_extn_ip'"/>/' /opt/freeswitch/conf/sip_profiles/external.xml
+sed -i 's/.*name="rtp-ip" value.*/<param name="rtp-ip" value="'$ctner_intn_ip'"\/>/' /opt/freeswitch/conf/sip_profiles/external.xml
+sed -i 's/.*name="sip-ip" value.*/<param name="sip-ip" value="'$ctner_intn_ip'"\/>/' /opt/freeswitch/conf/sip_profiles/external.xml
+sed -i 's/.*name="ext-rtp-ip" value.*/<param name="ext-rtp-ip" value="'$ctner_extn_ip'"\/>/' /opt/freeswitch/conf/sip_profiles/external.xml
+sed -i 's/.*name="ext-sip-ip" value.*/<param name="ext-sip-ip" value="'$ctner_extn_ip'"\/>/' /opt/freeswitch/conf/sip_profiles/external.xml
 #to change conf /usr/share/red5/webapps/sip/WEB-INF/bigbluebutton-sip.properties
-sed -i 's/.*bbb.sip.app.ip=.*/bbb.sip.app.ip=$ctner_intn_ip/' /usr/share/red5/webapps/sip/WEB-INF/bigbluebutton-sip.properties
-sed -i 's/.*freeswitch.ip=.*/freeswitch.ip=$ctner_intn_ip/' /usr/share/red5/webapps/sip/WEB-INF/bigbluebutton-sip.properties
+sed -i "s/.*bbb.sip.app.ip=.*/bbb.sip.app.ip=$ctner_intn_ip/" /usr/share/red5/webapps/sip/WEB-INF/bigbluebutton-sip.properties
+sed -i "s/.*freeswitch.ip=.*/freeswitch.ip=$ctner_intn_ip/" /usr/share/red5/webapps/sip/WEB-INF/bigbluebutton-sip.properties
 #to change /var/lib/tomcat7/webapps/demo/bbb_api_conf.jsp
 sed -i 's/.*String.BigBlueButtonURL.*/String BigBlueButtonURL = "http:\/\/'$ctner_extn_ip'\/bigbluebutton\/";/' /var/lib/tomcat7/webapps/demo/bbb_api_conf.jsp
 #to change /etc/nginx/sites-available/bigbluebutton
