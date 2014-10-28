3ba38481a96e2ee4c58dff7b36fc7fc079ebdfb3
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-28 09:21:13 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -10,9 +10,7 @@ if [ -f /etc/configured ]; then
         echo 'already configured'
 else
         #code that need to run only one time ....
-        #to finish install that fail when container image was created 
-        #container build image try to create the configuration but it is the wrong enviroment ... 
-       
+   
         #needed for fix problem with ubuntu and cron
         update-locale 
         date > /etc/configured
