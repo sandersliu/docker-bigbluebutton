113f56507e534eac30591629cbd1e08837fd8b7a
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-28 09:20:54 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -12,15 +12,7 @@ else
         #code that need to run only one time ....
         #to finish install that fail when container image was created 
         #container build image try to create the configuration but it is the wrong enviroment ... 
-        # but after it is create or run the first time is the best for it to finish it
-        source /root/.bashrc
-        #this fail under this eviurement but work fine under bash
-        apt-get update
-        apt-get install -y -q bbb-demo
-        #run the script to change ip
-        /sbin/after_install
-        #to find any error relate to the container configuration for future fix
-        bbb-conf --check  >>/var/log/bbb-conf.log 2>&1
+       
         #needed for fix problem with ubuntu and cron
         update-locale 
         date > /etc/configured
